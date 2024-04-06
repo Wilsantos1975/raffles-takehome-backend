4c24d6097c28b034c811014c06ff3324a2819e45
Your Name <Faustosantos@pursuit.org>
2024-04-06 11:02:47 -0400

    get raffle and get participants in one raffle routes created

--- controllers/rafflesControllers.js
+++ controllers/rafflesControllers.js
@@ -1,16 +1,31 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const raffleRouter = Router();
 
-const { 
-    getAllRaffles} 
-    = require('../queries/rafflesQueries');
+const { getAllRaffles, getRaffleById, getParticipantsByRaffleId } = require("../queries/rafflesQueries");
 // const { route } = require('../app');
 
 
-    raffleRouter.get('/', async (req, res) => {
-        const raffles = await getAllRaffles();
-        res.status(200).json({data: raffles});
-    }
-    );
+//get all raffles
+raffleRouter.get("/", async (req, res) => {
+  const raffles = await getAllRaffles();
+  res.status(200).json({ data: raffles });
+});
 
-    module.exports = raffleRouter;
+//get raffle by id
+raffleRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const raffle = await getRaffleById(id);
+  res.status(200).json({ data: raffle });
+});
+
+//get all participants for a raffle
+raffleRouter.get("/:id/participants", async (req, res) => {
+  const { id } = req.params;
+  const participants = await getParticipantsByRaffleId(id);
+  res.status(200).json({ data: participants });
+}); 
+
+
+
+
+module.exports = raffleRouter;
--- queries/rafflesQueries.js
+++ queries/rafflesQueries.js
@@ -1,13 +1,31 @@
+const { get } = require("../app");
 const db = require("../db/index");
 
 const getAllRaffles = async (req, res) => {
+  try {
+    const raffles = await db.any("SELECT * FROM raffles");
+    return raffles;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getRaffleById = async (id) => {
     try {
-        const raffles = await db.any("SELECT * FROM raffles");
-        return raffles
+        const raffle = await db.one("SELECT * FROM raffles WHERE id = $1", id);
+        return raffle;
     } catch (error) {
         return error;
     }
-}
+    };
 
+    getParticipantsByRaffleId = async (id) => {
+        try {
+            const participants = await db.any("SELECT * FROM participants WHERE raffle_id = $1", id);
+            return participants;
+        } catch (error) {
+            return error;
+        }
+    }
 
-module.exports = {getAllRaffles};
+module.exports = { getAllRaffles, getRaffleById, getParticipantsByRaffleId };
